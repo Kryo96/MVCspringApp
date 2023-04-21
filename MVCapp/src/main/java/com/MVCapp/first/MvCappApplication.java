aac73d10ef39740ebc174f774fec37bda41c55a3
Kryo96 <72083640+Kryo96@users.noreply.github.com>
2023-04-21 16:32:53 +0200

    Add files via upload

---
+++ MVCapp/src/main/java/com/MVCapp/first/MvCappApplication.java
@@ -0,0 +1,13 @@
+package com.MVCapp.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvCappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvCappApplication.class, args);
+	}
+
+}
---
+++ MVCapp/src/test/java/com/MVCapp/first/MvCappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MVCapp.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvCappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
