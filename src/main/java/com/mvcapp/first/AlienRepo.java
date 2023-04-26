8fe26614c2bbbf18c8062f1528c6a21a13da7e93
marco.marasciuolo <marco.marasciuolo@studenti.unimi.it>
2023-04-26 14:55:37 +0200

    fine

--- src/main/java/com/mvcapp/first/AlienRepo.java
+++ src/main/java/com/mvcapp/first/AlienRepo.java
@@ -1,7 +1,19 @@
 package com.mvcapp.first;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AlienRepo extends JpaRepository<Alien, Integer> {
 
+    //List<Alien> findByAname(String name); // Query DSL
+    // deve iniziare con findBy o getBy
+    //seguito dal nome della variabile che si cerca
+    //può anche essere seguito da un OrderBy
+    //findByAnameOrderByAidDesc in questo modo si ottiene la lista
+    //ordinata dei valori in tabella con quel nome.
+
+    @Query("from Alien where aname=")
+    List<Alien> find(String aname);
 }
